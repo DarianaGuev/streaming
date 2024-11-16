b87e34b5f59f4b3054a6d66a901fde5b4961ffa6
DarianaGuev <ddlossantos0306@gmail.com>
2024-11-15 20:08:02 -0600

    nuevos cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ['*.html', '*.js'],
+  content: ["./**/*.html"], //./*.html
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#a855f7', // Purple color in slider and icons
+        purple: '#8e44ad', // Customize to match the purple button color
+        lightGray: '#f0f0f0', // Background color of the card
+      info: {
+        primary: '#7c3aed',
+        'primary-dark': '#6d28d9'
+      }
+      },
+      borderRadius: {
+        lg: '12px', // Card and button border radius
+      },
+    },
   },
   plugins: [],
+ 
 }
 
